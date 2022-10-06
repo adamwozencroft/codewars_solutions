74e05e22e2e332ddfa6c7feec3530d9097a33c42
Adam Wozencroft <adamwozencroft@Kirsties-MacBook-Pro.local>
2022-10-06 15:27:29 +0100

    Sum of intervals codewars challenge

---
+++ sum_of_intervals/tests.py
@@ -0,0 +1,16 @@
+from sum_of_intervals import sum_of_intervals
+import unittest
+
+class Test_Roman_Numerals_Encoder(unittest.TestCase):
+
+    def test_inputs(self):
+        self.assertEqual(sum_of_intervals([(1, 5)]), 4)
+        self.assertEqual(sum_of_intervals([(1, 5), (6, 10)]), 8)
+        self.assertEqual(sum_of_intervals([(1, 5), (1, 5)]), 4)
+        self.assertEqual(sum_of_intervals([(1, 4), (7, 10), (3, 5)]), 7)
+        
+    def test_large_inputs(self):
+        self.assertEqual(sum_of_intervals([(-1_000_000_000, 1_000_000_000)]), 2_000_000_000)
+        self.assertEqual(sum_of_intervals([(0, 20), (-100_000_000, 10), (30, 40)]), 100_000_030)
+
+unittest.main()
